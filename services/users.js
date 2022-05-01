c260b0d8bcef013f19c8de7a7ad42c87fdd36c6e
DJorkaeff Peppard <djorkaeffpeppard@gmail.com>
2022-04-30 19:33:58 -0600

    Achivos del programa y correción en agregar usuario
    Archivos del rar del proyecto base
    Se agregó el campo de status al body para poder guardar sin porblemas

---
+++ services/users.js
@@ -0,0 +1,36 @@
+const usersModel = require('../models/users')
+
+/**
+ * Get an espcific user
+ * @param {number} pk_user User id
+ * @returns {{pk_user: 1, name: "Juan"}}
+ */
+const getUser = async (pk_user) => {
+    try {
+        return await usersModel.getUser(pk_user)
+    } catch (e) {
+        throw new Error(e.message)
+    }
+}
+
+/**
+ * Create an user
+ * @param {number} pk_user User id
+ * @param {string} name User name
+ * @param {boolean} status User status
+ * @returns {{pk_user: 1, name: "Juan", status:false}}
+ */
+const createUser = async (pk_user, name, status) => {
+    try {
+        return usersModel.createUser(pk_user, name, status)
+    } catch (e) {
+        throw new Error(e.message)
+    }
+}
+
+
+
+module.exports = {
+    getUser,
+    createUser
+}
