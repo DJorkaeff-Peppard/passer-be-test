6d955065c2e2cb1dd1acaf25bbc2296f1b1402c5
DJorkaeff Peppard <djorkaeffpeppard@gmail.com>
2022-04-30 21:22:13 -0600

    unit test para service users
    Se agrego el test para la funcion de updateUser y deleteUser

--- tests/services/users.services.test.js
+++ tests/services/users.services.test.js
@@ -2,8 +2,8 @@ const users = require('../../services/users')
 
 describe('users main functions', () => {
     // createUser function test
-    test('createUser with {pk_user: 5, name: "Carlos"}', async () => {
-        let user = await users.createUser(5, "Carlos");
+    test('createUser with {pk_user: 5, name: "Carlos", status:false}', async () => {
+        let user = await users.createUser(5, "Carlos",false);
         expect(user.pk_user).toBe(5);
     });
 
@@ -12,4 +12,16 @@ describe('users main functions', () => {
         let user = await users.getUser(123)
         expect(user.pk_user).toBe(123);
     });
+
+    // getUser function test
+    test('updateUser with {pk_user: 5, name: "Jose", status:true}', async () => {
+        let user = await users.updateUser(5,"Jose",true)
+        expect(user.pk_user).toBe(5);
+    });
+
+    test('deleteUser with {pk_user: 5}', async () => {
+        let user = await users.deleteUser(5)
+        expect(user.pk_user).toBe(5);
+    });
+
 })
