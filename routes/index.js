7ca8b5f4d1b0d082eb34cdba98290e477a380ba2
DJorkaeff Peppard <djorkaeffpeppard@gmail.com>
2022-05-01 14:11:06 -0600

    Creación de endpoint get transaction
    Se creó un endpoint que permite obtener una transaction especifica por pk_transaction

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ router.get('/users/:pk_user', users.getUser)
     .post('/users/', users.createUser)
     .put('/users/:pk_user',users.updateUser)
     .delete('/users/:pk_user',users.deleteUser)
+    .get('/transactions/:pk_transaction', transactions.getTransaction)
     .post('/transactions/', transactions.createTransaction)
 
 module.exports = router
