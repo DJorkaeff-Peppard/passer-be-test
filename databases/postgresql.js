96f1fdb2e8dda6896cc8e4ddb7877b7295a7f121
DJorkaeff Peppard <djorkaeffpeppard@gmail.com>
2022-04-30 20:13:26 -0600

    Creación de tabla transaction
    creación de una tabla transaction en postgresql

--- databases/postgresql.js
+++ databases/postgresql.js
@@ -4,7 +4,9 @@ const postgresql = newDb();
 
 // create mock data
 postgresql.public.none(`create table users(pk_user integer, name text, status boolean);
-                insert into users values (123, 'Juan', true);`);
+                        insert into users values (123, 'Juan', true);
+                        create table transactions(pk_transaction integer,fk_users integer, description text, amount float);
+                        insert into transactions values(1,123,'Compra',10.99)`);
 
 
 module.exports = {
